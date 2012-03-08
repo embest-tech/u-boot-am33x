5fdfe4d225485ac6a63b874143f1a784d5394f55
Vaibhav Hiremath <hvaibhav@ti.com>
2012-03-08 17:15:47 +0530

    am335x: Enable RTC 32K OSC clock
    In order to support low power state, you must source kernel system
    timers to persistent clock, available across suspend/resume.  In case of
    AM335x device, the only source we have is, RTC32K, available in
    wakeup/always-on domain.  Having said that, during validation it has
    been observed that, RTC clock need couple of seconds delay to stabilize
    the RTC OSC clock; and such a huge delay is not acceptable in kernel
    especially during early init and also it will impact quick/fast boot
    use-cases.
    
    So, RTC32k OSC enable dependency has been shifted to
    SPL/first-bootloader.
    
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>
    Signed-off-by: Tom Rini <trini@ti.com>
    
    Resolved conflicts when merging from upstream:
    
    	arch/arm/cpu/armv7/am33xx/board.c
    	arch/arm/cpu/armv7/am33xx/clock.c
    	arch/arm/include/asm/arch-am33xx/hardware.h
    
    Signed-off-by: Tom Rini <trini@ti.com>

--- arch/arm/include/asm/arch-am33xx/hardware.h
+++ arch/arm/include/asm/arch-am33xx/hardware.h
@@ -61,6 +61,7 @@
 #define CM_WKUP				0x44E00400
 #define CM_DPLL				0x44E00500
 #define CM_DEVICE			0x44E00700
+#define CM_RTC				0x44E00800
 #define CM_CEFUSE			0x44E00A00
 #define PRM_DEVICE			0x44E00F00
 
@@ -89,5 +90,8 @@
 /* OTG */
 #define AM335X_USB0_OTG_BASE		0x47401000
 #define AM335X_USB1_OTG_BASE		0x47401800
+
+/* RTC base address */
+#define AM335X_RTC_BASE			0x44E3E000
 
 #endif /* __AM33XX_HARDWARE_H */
