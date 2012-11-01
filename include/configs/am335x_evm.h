yanok/u-boot

am335x_evm: add restore_flash config option
Add new am335x_evm_restore_flash configuration (to be used with
netboot SPL). In this configuration U-Boot will try to load and
run debrick.img script automatically after boot.

Signed-off-by: Ilya Yanok <ilya.yanok@cogentembedded.com>

include/configs/am335x_evm.h
@@ -39,9 +39,6 @@
 #define CONFIG_SETUP_MEMORY_TAGS
 #define CONFIG_INITRD_TAG		/* Required for ramdisk support */
 
-/* set to negative value for no autoboot */
-#define CONFIG_BOOTDELAY		3
-
 #define CONFIG_MMC
 #define CONFIG_NAND
 #define CONFIG_SPI
@@ -127,6 +124,10 @@
 		"run net_args; " \
 		"bootm ${kloadaddr}\0" \
 
+#ifndef CONFIG_RESTORE_FLASH
+/* set to negative value for no autoboot */
+#define CONFIG_BOOTDELAY		3
+
 #define CONFIG_BOOTCOMMAND \
 	"if mmc rescan; then " \
 		"echo SD/MMC found on device ${mmc_dev};" \
@@ -144,6 +145,17 @@
 		"fi;" \
 	"fi;" \
 	"run nand_boot;" \
+
+#else
+#define CONFIG_BOOTDELAY		0
+
+#define CONFIG_BOOTCOMMAND			\
+	"setenv autoload no; "			\
+	"dhcp; "				\
+	"if tftp 80000000 debrick.img; then "	\
+		"source 80000000; "		\
+	"fi"
+#endif
 
 #define CONFIG_MISC_INIT_R
 #define CONFIG_SYS_AUTOLOAD		"yes"

embest-tech/u-boot-am33x

Signed-off-by: Tom Rini <trini@ti.com>

include/configs/am335x_evm.h
@@ -44,8 +44,6 @@
 #define CONFIG_CMD_ASKENV
 #define CONFIG_VERSION_VARIABLE
 
-/* set to negative value for no autoboot */
-#define CONFIG_BOOTDELAY		1
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"loadaddr=0x80200000\0" \
 	"fdtaddr=0x80F80000\0" \
@@ -80,6 +78,10 @@
 		"run ramargs; " \
 		"bootm ${loadaddr}\0" \
 
+#ifndef CONFIG_RESTORE_FLASH
+/* set to negative value for no autoboot */
+#define CONFIG_BOOTDELAY		3
+
 #define CONFIG_BOOTCOMMAND \
 	"if mmc rescan ${mmcdev}; then " \
 		"echo SD/MMC found on device ${mmcdev};" \
@@ -95,6 +97,17 @@
 			"run mmcboot;" \
 		"fi;" \
 	"fi;" \
+
+#else
+#define CONFIG_BOOTDELAY		0
+
+#define CONFIG_BOOTCOMMAND			\
+	"setenv autoload no; "			\
+	"dhcp; "				\
+	"if tftp 80000000 debrick.scr; then "	\
+		"source 80000000; "		\
+	"fi"
+#endif
 
 /* Clock Defines */
 #define V_OSCK				24000000  /* Clock output from T2 */